8a4ca424a5e49286f5d0d7f013fb2870dfa51b29
Keifer.Gu <keifergu@gmail.com>
2016-09-11 22:58:41 +0800

    添加对圆和矩形的碰撞检测

--- src/module/graphical.js
+++ src/module/graphical.js
@@ -6,6 +6,10 @@ var Rect = function (x, y, width, height) {
 	this.width  = width;
 	this.height = height;
 	this.color  = 'black';
+	this.points = [[this.x, this.y],
+								 [this.x + this.width, y],
+								 [this.x + this.width, this.y + this.height],
+								 [this.x, this.y + this.height]];
 }
 
 //设置颜色
@@ -60,8 +64,46 @@ Circle.prototype.set = function (obj) {
 	}
 	return this;
 }
+
 Circle.prototype.move = function () {
 	this.x += this.vx;
 	this.y += this.vy;
 	return this;
+}
+
+Circle.prototype.collision = function (graphical) {
+	if (graphical instanceof Rect) {
+		var rect = graphical,
+			rectPoints = [],
+			rectVector = [], //各条边的向量的法向量的单位向量
+			produnctLength = [],  //储存多边形相对于单位向量的点积长度
+			circleProdunct;
+
+		var circleVector = new Vector(this.x, this.y);
+
+		rectPoints[0] = new Vector(rect.x, rect.y);
+		rectPoints[1] = new Vector(rect.x + rect.width, rect.y);
+		rectPoints[2] = new Vector(rect.x + rect.width, rect.y + rect.height);
+		rectPoints[3] = new Vector(rect.x, rect.y + rect.height);
+
+		//计算矩形各个边的向量
+		for (var i = 0; i + 1 < rectPoints.length; i++) {
+			rectVector[i] = rectPoints[i+1].substract(rectPoints[i]).prependicular().normalize();
+		}
+		rectVector[3] = rectPoints[0].substract(rectPoints[3]).prependicular().normalize();
+
+		for (var i = 0; i < rectPoints.length; i++) {
+			circleProdunct = circleVector.dotProduct(rectVector[i]);  //计算圆心和单位向量的点积
+			//对矩形上的每个点求点积
+			rectPoints.forEach(function (point, key) {
+				produnctLength.push(point.dotProduct(rectVector[i]));
+			})
+			produnctLength.sort();
+			if (circleProdunct + this.r >= produnctLength[0] || circleProdunct - this.r <= produnctLength[produnctLength.length]) {
+				return true;
+			} else {
+				return false;
+			}
+		}		
+	}
 }
