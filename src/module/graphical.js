45dd56f09c4879e36fcb02f136fac61161471d68
Keifer.Gu <keifergu@gmail.com>
2016-09-13 12:35:13 +0800

    使用es6重写，安装eslint

--- src/module/graphical.js
+++ src/module/graphical.js
@@ -1,4 +1,13 @@
-var Rect = function (x, y, width, height) {
+class Shape {
+	constructor(args) {
+		// code
+	}
+
+	// methods
+}
+class Rect extends Shape {
+
+ 	constructor(x, y, width, height) {
 	this.x = x;
 	this.y = y;
 	this.vx = 0;
@@ -10,100 +19,101 @@ var Rect = function (x, y, width, height) {
 								 [this.x + this.width, y],
 								 [this.x + this.width, this.y + this.height],
 								 [this.x, this.y + this.height]];
-}
-
-//设置颜色
-Rect.prototype.setColor = function (color) {
-	this.colore = color;
-}
-
-//绘制矩形
-Rect.prototype.draw = function (ctx) {
-	ctx.beginPath();
-	ctx.fillStyle = this.color;
-	ctx.fillRect(this.x, this.y, this.width, this.height);
-	ctx.closePath();
-}
-
-Rect.prototype.set = function (obj) {
-	var objArr = Object.keys(obj);
-	for(var key in objArr) {
-		this[objArr[key]] = obj[objArr[key]];
 	}
-	return this;
-}
 
-Rect.prototype.move = function () {
-	this.x += this.vx;
-	this.y += this.vy;
-	return this;
-}
-
-var Circle = function (x, y, r) {
-	this.x = x;
-	this.y = y;
-	this.r = r;
-	this.vx = 0;
-	this.vy = 0;
-	this.color = 'blcak';
-}
-
-Circle.prototype.draw = function (ctx) {
-	ctx.beginPath();
-	ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, true);
-	ctx.closePath();
-	ctx.fillStyle = this.color;
-	ctx.fill();
-	return this;
-}
-
-Circle.prototype.set = function (obj) {
-	var objArr = Object.keys(obj);
-	for(var key in objArr) {
-		this[objArr[key]] = obj[objArr[key]];
+	setColor(color) {
+		this.colore = color;
 	}
-	return this;
-}
 
-Circle.prototype.move = function () {
-	this.x += this.vx;
-	this.y += this.vy;
-	return this;
-}
+	draw(ctx) {
+		ctx.beginPath();
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.closePath();
+	}
 
-Circle.prototype.collision = function (graphical) {
-	if (graphical instanceof Rect) {
-		var rect = graphical,
-			rectPoints = [],
-			rectVector = [], //各条边的向量的法向量的单位向量
-			produnctLength = [],  //储存多边形相对于单位向量的点积长度
-			circleProdunct;
-
-		var circleVector = new Vector(this.x, this.y);
-
-		rectPoints[0] = new Vector(rect.x, rect.y);
-		rectPoints[1] = new Vector(rect.x + rect.width, rect.y);
-		rectPoints[2] = new Vector(rect.x + rect.width, rect.y + rect.height);
-		rectPoints[3] = new Vector(rect.x, rect.y + rect.height);
-
-		//计算矩形各个边的向量
-		for (var i = 0; i + 1 < rectPoints.length; i++) {
-			rectVector[i] = rectPoints[i+1].substract(rectPoints[i]).prependicular().normalize();
+	set(obj) {
+		var objArr = Object.keys(obj);
+		for(var key in objArr) {
+			this[objArr[key]] = obj[objArr[key]];
 		}
-		rectVector[3] = rectPoints[0].substract(rectPoints[3]).prependicular().normalize();
+		return this;
+	}
 
-		for (var i = 0; i < rectPoints.length; i++) {
-			circleProdunct = circleVector.dotProduct(rectVector[i]);  //计算圆心和单位向量的点积
-			//对矩形上的每个点求点积
-			rectPoints.forEach(function (point, key) {
-				produnctLength.push(point.dotProduct(rectVector[i]));
-			})
-			produnctLength.sort();
-			if (circleProdunct + this.r >= produnctLength[0] || circleProdunct - this.r <= produnctLength[produnctLength.length]) {
-				return true;
-			} else {
-				return false;
+	move() {
+		this.x += this.vx;
+		this.y += this.vy;
+		return this;
+	}
+}
+
+class Circle extends Shape {
+	constructor(x, y, r) {
+		this.x = x;
+		this.y = y;
+		this.r = r;
+		this.vx = 0;
+		this.vy = 0;
+		this.color = 'blcak';
+	}
+
+	draw(ctx) {
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, true);
+		ctx.closePath();
+		ctx.fillStyle = this.color;
+		ctx.fill();
+		return this;
+	}
+
+	set(obj) {
+		var objArr = Object.keys(obj);
+		for(var key in objArr) {
+			this[objArr[key]] = obj[objArr[key]];
+		}
+		return this;
+	}
+
+	move() {
+		this.x += this.vx;
+		this.y += this.vy;
+		return this;
+	}
+
+	collision(graphical) {
+		if (graphical instanceof Rect) {
+			var rect = graphical,
+				rectPoints = [],
+				rectVector = [], //各条边的向量的法向量的单位向量
+				produnctLength = [],  //储存多边形相对于单位向量的点积长度
+				circleProdunct;
+
+			var circleVector = new Vector(this.x, this.y);
+
+			rectPoints[0] = new Vector(rect.x, rect.y);
+			rectPoints[1] = new Vector(rect.x + rect.width, rect.y);
+			rectPoints[2] = new Vector(rect.x + rect.width, rect.y + rect.height);
+			rectPoints[3] = new Vector(rect.x, rect.y + rect.height);
+
+			//计算矩形各个边的向量
+			for (var i = 0; i + 1 < rectPoints.length; i++) {
+				rectVector[i] = rectPoints[i+1].substract(rectPoints[i]).prependicular().normalize();
 			}
-		}		
+			rectVector[3] = rectPoints[0].substract(rectPoints[3]).prependicular().normalize();
+
+			for (var i = 0; i < rectPoints.length; i++) {
+				circleProdunct = circleVector.dotProduct(rectVector[i]);  //计算圆心和单位向量的点积
+				//对矩形上的每个点求点积
+				rectPoints.forEach(function (point, key) {
+					produnctLength.push(point.dotProduct(rectVector[i]));
+				})
+				produnctLength.sort();
+				if (circleProdunct + this.r >= produnctLength[0] || circleProdunct - this.r <= produnctLength[produnctLength.length]) {
+					return true;
+				} else {
+					return false;
+				}
+			}		
+		}
 	}
 }
--- src/module/vector.js
+++ src/module/vector.js
@@ -1,35 +1,32 @@
-var Vector = function (x, y) {
-	this.x = x;
-	this.y = y;
-};
-
-//向量求差
-Vector.prototype.substract = function (vector) {
-	return new Vector(this.x - vector.x, this.y - vector.y);
-};
-
-//求向量的法向量
-Vector.prototype.prependicular = function () {
-	return new Vector(this.y, -this.x);
-};
-
-//求向量长度
-Vector.prototype.getMagnitude = function () {
-	return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
-};
-
-//求单位向量
-Vector.prototype.normalize = function () {
-	var v = new Vector(0, 0),
-		m = this.getMagnitude();
-	if (m != 0) {
-		v.x = this.x / m;
-		v.y = this.y / m;
+class Vector {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
 	}
-	return v;
-};
 
-//求矢量点积
-Vector.prototype.dotProduct = function (vector) {
-	return this.x * vector.x + this.y * vector.y;
-};
+	substract(vector) {
+		return new Vector(this.x - vector.x, this.y - vector.y);
+	}
+
+	prependicular() {
+		return new Vector(this.y, -this.x);
+	}
+
+	getMagnitude() {
+		return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+	}
+
+	normalize() {
+		var v = new Vector(0, 0),
+			m = this.getMagnitude();
+		if (m != 0) {
+			v.x = this.x / m;
+			v.y = this.y / m;
+		}
+		return v;
+	}
+	
+	dotProduct(vector) {
+		return this.x * vector.x + this.y * vector.y;
+	}
+}
