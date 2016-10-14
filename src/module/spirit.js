e641db93e4df64d969a23685b9e9fc57afd2fbb0
Keifer.Gu <keifergu@gmail.com>
2016-10-14 18:12:12 +0800

    完善spirit类，修改move的bug，添加action方法

--- src/module/spirit.js
+++ src/module/spirit.js
@@ -2,22 +2,36 @@ import {Shape, Polygon, Circle}  from "./graphical.js";
 import * as Collision from "collision.js";
 
 let spiritShapeWord = {
-	polygon: "polygon",
-	circle: "circle",
 	rect: "rect",
-}
+	circle: "circle",
+	polygon: "polygon",
+};
 
 let moveWord = {
-	left: "left",
-	right: "right",
 	up: "up",
+	left: "left",
 	down: "down",
-}
+	stop: "stop",
+	right: "right",
+};
+
+let defaultAction = {
+	jump: "jump", 
+};
+
+let actionContainer = {};
+
+Spirit.addAction(defaultAction.jump, (distance = 10) => {
+	let ySpeed = Math.sqrt(2 * distance * this._spirit.speed.ay);
+	this._spirit.speed.vy = ySpeed;
+})
+
 export default class Spirit {
-	constructor({shape: spiritShapeWord.rect, base: [0, 0, 10, 10], img, context}) {
-		this.shape = shape;
+	constructor({shape = spiritShapeWord.rect, base = [0, 0, 10, 10], speed = 5, img, context}) {
 		this.base = base;
-		this.ctx = context;
+		this.shape = shape;
+		this.speed = speed;
+		this.context = context;
 		this._spirit = undefined;
 		if (img !== null) {
 			this.img = new Image();
@@ -26,45 +40,60 @@ export default class Spirit {
 	}
 	
 	draw() {
-		if (this.context === undefined) {
+		let	_img = this.img,
+			_base = this.base,
+		  _spirit = this._spirit,
+			_context = this.context;
+		if (_context === undefined) {
 			throw "must init the canvas context";
 		}
 		//当spirit没有创建实例时，新建实例
-		if (this._spirit === undefined) {
+		if (_spirit === undefined) {
 			switch(this.shape) {
 				case spiritShapeWord.rect:
-					this._spirit = new Rect(...this.base);
+					this._spirit = new Rect(..._base);
 			}
 		}
 		//存在图像则绘制图像，只加载一次图像
-		if (this.img !== undefined) {
-			this.ctx.drawImage(this.img, base[0], base[1]);
+		if (_img !== undefined) {
+			_img.onload = () => {
+				_context.drawImage(_img, _base[0], _base[1]);
+			}
 		}
-		let _spirit = this._spirit;
-		_spirit.draw(this.ctx);
+		_spirit.draw(_context);
 	}
 
 	move(direction = moveWord.right) {
-		let _speed = {
-			vx: Math.abs(this._spirit.speed.vx),
-			vy: Math.abs(this._spirit.speed.vy)
-		}
+		let _speed = this.speed;
 		switch(direction) {
 			case moveWord.left:
-				this._spirit.speed.vx = - _speed.vx;
+				this._spirit.speed = {vx: -_speed};
 				break;
 			case moveWord.right:
-				this._spirit.speed.vx = _speed.vx;
+				this._spirit.speed = {vx: _speed};
 				break;
 			case moveWord.up:
-				this._spirit.speed.vy = - _speed.vy;
+				this._spirit.speed = {vy: -_speed};
 				break;
-			case moveWord.left:
-				this._spirit.speed.vy = _speed.vy;
+			case moveWord.down:
+				this._spirit.speed = {vy: -_speed};
+				break;
+			case moveWord.stop:
+				this._spirit.speed = {vx: 0, vy: 0};
+			default:
+				throw "must specify a move action";
 				break;
 		}
+		this._spirit.move();
 	}
 
+	action(type = "") {
+		return actionContainer[type];
+	}
+
+	static addAction(type, fun) {
+		actionContainer[type] = fun;
+	}
 	get bottom() {
 		return this._spirit.bottom();
 	}
